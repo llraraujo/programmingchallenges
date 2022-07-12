f4296b3db2f6b4a2f1b757d720f8f4699abaf1bb
lucas <lucasregoaraujo@gmail.com>
2022-07-11 23:36:55 -0300

    :tada: Problem1154 solved

---
+++ beecrowd/BeecrowdCSharp/BeecrowdCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+namespace BeecrowdCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ages = new List<int>();
+            while (true)
+            {
+                int age = int.Parse(Console.ReadLine());
+                if (age < 0) break;
+                ages.Add(age);
+            }
+            double media = (double) ages.Sum() / ages.Count();
+
+            Console.Write(media.ToString("F2", CultureInfo.InvariantCulture));
+            
+
+        }
+    }
+}
