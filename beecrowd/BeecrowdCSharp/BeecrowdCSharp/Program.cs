86c6f8c217418ab196eb76942cb162ebc94766a1
lucas <lucasregoaraujo@gmail.com>
2022-07-12 23:17:56 -0300

    :tada: problem1158 solved.

--- beecrowd/BeecrowdCSharp/BeecrowdCSharp/Program.cs
+++ beecrowd/BeecrowdCSharp/BeecrowdCSharp/Program.cs
@@ -8,13 +8,37 @@ namespace BeecrowdCSharp
     class Program
     {
         static void Main(string[] args)
-        {
-            int n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            
-            for(int i = 1; i <= n; i++)
+        {   
+         
+            int tests = int.Parse(Console.ReadLine());
+            for(int i = 0; i < tests; i++)
             {
-                if (n % i == 0) Console.WriteLine(i);
+                string[] entrada = Console.ReadLine().Split(' ');
+                int x = int.Parse(entrada[0]);
+                int y = int.Parse(entrada[1]);
+
+                if (x % 2 == 0) x += 1;
+
+                int soma = 0;
+                int count = 0;
+
+                while (true)
+                {
+                    if (x % 2 != 0)
+                    {
+                        soma += x;
+                        count += 1;
+                    }
+
+                    if (count == y) break;
+
+                    x += 1;
+
+                }
+                Console.WriteLine(soma);
+
             }
+
 
         }
     }
