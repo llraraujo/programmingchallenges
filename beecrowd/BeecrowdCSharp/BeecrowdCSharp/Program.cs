ca650094b646e154ee758e7bcc09d60a0bc8e78c
= <lucasregoaraujo@gmail.com>
2023-03-20 19:15:57 -0300

    :tada: problem 1177 solved

--- beecrowd/BeecrowdCSharp/BeecrowdCSharp/Program.cs
+++ beecrowd/BeecrowdCSharp/BeecrowdCSharp/Program.cs
@@ -9,28 +9,26 @@ namespace BeecrowdCSharp
     {
         static void Main(string[] args)
         {
-            int quantidadeDeTestes = int.Parse(Console.ReadLine());
+            int valor = int.Parse(Console.ReadLine());
+            int[] vetor = new int[1000];
+            int aux = 0;
 
-            for(int t = 0; t < quantidadeDeTestes; t++)
+            for(int t = 0; t < vetor.Length; t++)
             {
-                int posicao = int.Parse(Console.ReadLine());
-                Console.WriteLine("Fib(" + posicao + ") = " + Fibonacci(posicao));
+                if(t > 1 && (t % valor) == 0)
+                {
+                    aux = 0;
+                    vetor[t] = aux;
+                    Console.WriteLine("N[" + t + "] = " + vetor[t]);
+                }
+                else
+                {
+                    vetor[t] = aux;
+                    Console.WriteLine("N[" + t + "] = " + vetor[t]);
+                }
+                aux++;
             }
                    
-        }
-
-        private static long Fibonacci(int posicao)
-        {
-            if (posicao == 0) return 0;
-            if (posicao == 1) return 1;
-            List<long> fibonacci = new List<long>() { 0, 1 };
-            while(posicao >= fibonacci.Count)
-            {
-                long ultimo = fibonacci[fibonacci.Count - 1];
-                long penultimo = fibonacci[fibonacci.Count - 2];
-                fibonacci.Add(ultimo + penultimo);
-            }
-            return fibonacci[posicao];           
         }
     }
 }
